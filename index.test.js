0220350cec03b5c75bbf95277f4634bc839c9513
lheumell-o <h.leo@odocar.com>
2026-09-25 15:45:17 +0200

    feat(CI): update comment PR

--- index.test.js
+++ index.test.js
@@ -2,6 +2,6 @@ const test = require('node:test')
 const assert = require('node:assert')
 const { additionner } = require('./index')
 
-test('2 + 3 vaut 5', () => {
-  assert.strictEqual(additionner(2, 3), 5)
+test('2 + 3 vaut 6', () => {
+  assert.strictEqual(additionner(2, 3), 6)
 })
